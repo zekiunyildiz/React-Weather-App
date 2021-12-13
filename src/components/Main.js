afeb67c8ece661f735c6d14c2e119c99384fe80d
zekiunyildiz <zekiunyildiz@gmail.com>
2021-12-13 16:20:07 +0300

    added axios and API weather

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import axios from 'axios';
 import Header from './Header'
 
+const API_KEY = "43c7ee22c8621489263185308bf448d3";
+
+
 function Main() {
+    const api_call = async () => {
+        const url = `api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=${API_KEY}`
+        const request = axios.get(url)
+        const response = await request
+        console.log(response);
+    }
     return (
         <div className='main'>
             <Header/>
