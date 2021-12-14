97eae5dbfb20304c624eae72f67b8cd65b15c977
zekiunyildiz <zekiunyildiz@gmail.com>
2021-12-15 01:40:05 +0300

    usestate

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Header from "./Header";
 import Content from "./Content";
 import WeatherSearch from "./WeatherSearch";
 
-
-
 const Main = () => {
-  const api_call = async e => {
+  const [weather, setWeather] = useState()
+  const api_call = async (e) => {
     e.preventDefault();
     const API_KEY = "43c7ee22c8621489263185308bf448d3";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=${API_KEY}`;
     const request = axios.get(url);
     const response = await request;
-    console.log(response);
+    setWeather(response);
   };
-
-  
+  console.log(weather);
 
   return (
     <div className="main">
