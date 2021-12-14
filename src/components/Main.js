ddcc525c04440e35b4663a086274d760c9b49e7d
zekiunyildiz <zekiunyildiz@gmail.com>
2021-12-14 14:34:20 +0300

    call api_call function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import axios from 'axios';
-import Header from './Header'
+import React, { useEffect } from "react";
+import axios from "axios";
+import Header from "./Header";
+import Content from "./Content";
+import WeatherSearch from "./WeatherSearch";
 
-const API_KEY = "43c7ee22c8621489263185308bf448d3";
 
 
-function Main() {
-    const api_call = async () => {
-        const url = `api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=${API_KEY}`
-        const request = axios.get(url)
-        const response = await request
-        console.log(response);
-    }
-    return (
-        <div className='main'>
-            <Header/>
-        </div>
-    )
-}
+const Main = () => {
+  const api_call = async e => {
+    e.preventDefault();
+    const API_KEY = "43c7ee22c8621489263185308bf448d3";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=${API_KEY}`;
+    const request = axios.get(url);
+    const response = await request;
+    console.log(response);
+  };
 
-export default Main
+  
 
+  return (
+    <div className="main">
+      <Header />
+      <Content>
+        <WeatherSearch api_call={api_call} />
+      </Content>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/WeatherSearch.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const WeatherSearch = (props) => {
+  return (
+    <div className="weather-search">
+      <form onSubmit={props.api_call} className="weather-search__form">
+        <input
+          autoComplete="off"
+          className="weather-search__input"
+          type="text"
+        />
+        <div className="weather-search__submit">
+          <button className="weather-search__button">&rarr;</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default WeatherSearch;
